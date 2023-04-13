51251e05495cfcd313b7a4d0c193290bab668630
siavava <amittaijoel@outlook.com>
2023-04-12 23:07:36 -0400

    it's gucci, or am I?

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 /**
  * @author Amittai (siavava)
- * 
+ *
  */
 
+// imports
+import $ from 'jquery';
+import './style.scss';
 
-console.log("Starting up...")
+console.log('Starting up...');
+$('#main').html('Here we go!');
+
+let count = 0;
+const renderCount = () => {
+  $('#main').html(`You have been on this page for ${count} seconds.`);
+};
+
+const tick = () => {
+  count += 1;
+  renderCount();
+};
+
+setInterval(tick, 1000);
