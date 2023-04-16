32cd9d344f132474914c35fa4657cb791a49a1b3
siavava <amittaijoel@outlook.com>
2023-04-16 19:01:06 -0400

    refactor / clean-up starter

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom';
 import './style.scss';
+// import $ from 'jquery';
 
 function App() {
   return <div className="test">All the REACT are belong to us!</div>;
@@ -8,3 +9,16 @@ function App() {
 
 const root = createRoot(document.getElementById('main'));
 root.render(<App />);
+
+// code to follow mouse... from ported from the starter.
+const tracker = document.getElementById('tracker');
+document.body.onpointermove = (event) => {
+  const { pageX, pageY } = event;
+  tracker.animate({
+    top: `${pageY - 200}px`,
+    left: `${pageX - 200}px`,
+  }, {
+    duration: 3000,
+    fill: 'forwards',
+  }, 'ease-in-out');
+};
