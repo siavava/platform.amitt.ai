8057760d2030035a15e5f83e8444acdb3a55357e
siavava <amittaijoel@outlook.com>
2023-04-19 23:15:27 -0400

    routers

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,53 @@
 import React from 'react';
-import { createRoot } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './style.scss';
-// import $ from 'jquery';
+import {
+  BrowserRouter, Routes, Route, NavLink, useParams,
+} from 'react-router-dom';
 
 function App() {
-  return <div className="test">All the REACT are belong to us!</div>;
+  return (
+    <BrowserRouter>
+      <div className="app-container">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/test/:id" element={<Test />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 const root = createRoot(document.getElementById('main'));
 root.render(<App />);
+
+function About(props) {
+  return <div className="route-text"> All there is to know about me </div>;
+}
+
+function Welcome(props) {
+  return <div className="route-text"> Welcome to my site. </div>;
+}
+
+function Nav(props) {
+  return (
+    <nav className="nav">
+      <ul className="nav-links">
+        <li><NavLink className="nav-link" to="/">Home</NavLink></li>
+        <li><NavLink className="nav-link" to="/about">About</NavLink></li>
+        <li><NavLink className="nav-link" to="/test/id1">test id1</NavLink></li>
+        <li><NavLink className="nav-link" to="/test/id2">test id2</NavLink></li>
+      </ul>
+    </nav>
+  );
+}
+
+function Test(props) {
+  const { id } = useParams();
+  return <div className="route-text"> This is a test route with ID {id} </div>;
+}
 
 // code to follow mouse... from ported from the starter.
 const tracker = document.getElementById('tracker');
